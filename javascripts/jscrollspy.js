d31b76ff2b113aececb06c73248b34a18f25c26c
will chan <willchan2@gmail.com>
2013-07-15 21:54:51 -0400

    initial commit

---
+++ javascripts/jscrollspy.js
@@ -0,0 +1,29 @@
+;(function( $ ) {
+  'use strict';
+
+  var jScrollspy = {
+    init: function(selector, options) {
+      this.selector  = selector;
+      this.$selector = $(selector);
+      this.options   = $.extend({}, $.fn.jScrollspy.options, options);
+
+      this.setupEvents();
+    },
+    setupEvents: function() {
+      $(document).on('click', this.$selector, function(event) {
+        event.preventDefault();
+        alert('hello');
+      });
+    }
+  };
+
+  $.fn.jScrollspy.options = {};
+  $.fn.jScrollspy = function(options) {
+    var self = this;
+
+    return this.each(function() {
+      var scrollspy = Object.create(jScrollspy);
+      scrollspy.init(self.selector, options);
+    });
+  };
+}( jQuery ));
